85d4c4ebd0ffd7ff43efe881233e5eff4534b9c1
cubeli(李勇) <cubeli@tencent.com>
2017-06-21 16:58:59 +0800

    add

---
+++ bin/orcreader.sh
@@ -0,0 +1,2 @@
+#java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8449 -classpath `find lib/*| grep -v source | tr '\n' ':'` com.tencent.cubeli.orc.ORCWriterTest /data1/cubeli/orctest/tpch_2_17_0/dbgen/lineitem.tbl
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8449 -classpath `find lib/*| grep -v source | tr '\n' ':'`:conf com.tencent.cubeli.orc.ORCReaderTest
